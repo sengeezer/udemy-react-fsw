c6bc81be260bb6716c8a34ccd1d8209535238cc1
Fabio Albertin <albertin@gmail.com>
2017-09-23 00:25:08 +0100

    Better message

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ hullo: 'thurr' });
 });
 
 app.listen(PORT);
