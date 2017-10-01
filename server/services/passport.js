506aa5205e1a2cb986a5fa3a665e2713569affaf
Fabio Albertin <albertin@gmail.com>
2017-10-01 02:28:23 +0100

    Finished lecture 40

--- server/services/passport.js
+++ server/services/passport.js
@@ -5,12 +5,31 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+});
+
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback',
 }, (accessToken, refreshToken, profile, done) => {
   // refresh (access) token
-  new User({ googleID: profile.id }).save();
+  User.findOne({ googleID: profile.id }).then((existingUser) => {
+    if (existingUser) {
+      // status, record
+      done(null, existingUser);
+    } else {
+      new User({ googleID: profile.id }).save().then((user) => {
+        done(null, user);
+      });
+    }
+  });
 }),
 );
