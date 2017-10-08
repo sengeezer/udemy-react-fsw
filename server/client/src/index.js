241125a79ec647efd0516700d83bc6f5e3895970
Fabio Albertin <albertin@gmail.com>
2017-10-08 23:37:34 +0100

    Starting to add redux

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
 
